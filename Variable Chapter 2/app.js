346553e2eac999a09554515fd325e1bb586a4d72
Bayezid Hossain <bayezidhossain086@gmail.com>
2025-05-08 23:13:43 +0600

    Variable Chapter 02

---
+++ Variable Chapter 2/app.js
@@ -0,0 +1,205 @@
+//  Star const 35 Page
+const weight = 62 ;
+const price = 760 ;
+const year = 2025 ;
+const balance = 1000 ;
+const age = 18 ;
+// Practice
+const birth = 17-11-2007 ;
+const family = 4 ;
+const students = 350 ;
+const marks = 240 ;
+const rooms = 5 ;
+const academic = 2 ;
+const total = 35 ;
+
+// Start console 38 Page
+// Practice
+console.log(age);
+
+const booksToRead = 5 ;
+console.log(booksToRead);
+
+const wakeUpTime = 5.10 ;
+console.log(wakeUpTime);
+
+const wastedTime = 1.5 ;
+console.log(wastedTime)
+
+const borrow = 0 ;
+console.log(borrow);
+
+const goals = 2 ;
+console.log(goals);
+
+const pillow = 2 ;
+console.log(pillow);
+
+// start String 39 Page
+const myself = "Bayezid Hossain";
+const  address = "Lakshmipur , Bangladesh";
+const favFood = "Biryani";
+const hidingPlace = "Bed or niche";
+const drinking = "Tea" ;
+const subject = "English" ;
+const teacher = "Dragon Delwar";
+const phone = "Samsung" ;
+const foood = "watermelon";
+// Practice 
+console.log(myself);
+console.log(address);
+
+const college = "Lakshmipur Govt. College";
+console.log(college);
+
+const fan = "Mridul";
+console.log(fan);
+
+const movieOfTheYear = "Family Entertainment Bd YouTube Channel";
+console.log(movieOfTheYear);
+
+const hobby = "Gerdening";
+console.log(hobby);
+
+const color = "Blue";
+console.log(color);
+
+// boolean true false Page 41
+
+const moneyMoved = true ;
+const learning = true ;
+const isHomeworkDone = false ;
+const isCrushReplied = false ;
+const hasPhoneBattery = false ;
+const singleForever = true ;
+
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                
+const isRich = true ;
+console.log(isRich);
+
+const passed = false ;
+console.log(passed);
+// Practice
+const examPassed = true ;
+console.log(examPassed);
+
+const crush = false ;
+console.log(crush);
+
+const celebrating  = true ;
+console.log(celebrating);
+
+const isHappy = true ;
+console.log(isHappy);
+
+const hasMoney = false ;
+console.log(hasMoney);
+
+const isHungry = true ;
+console.log(isHungry);
+
+const isStudyDone = true ;
+console.log(isStudyDone);
+
+const isPhoneCharged = true;
+console.log(isPhoneCharged);
+
+const isDinnerDone = true;
+console.log(isDinnerDone);
+
+// Start গনিতের গ্যাংষ্টার 43 Page
+const eggPrice = 10 ;
+const onionPrice = 20 ;
+const tetal = eggPrice + onionPrice ;
+console.log(tetal);
+const result = (eggPrice - onionPrice) ;
+console.log(result);
+
+console.log(onionPrice - eggPrice);
+
+console.log(onionPrice * eggPrice);
+
+console.log(onionPrice / eggPrice);
+
+console.log(10 / 3);
+
+console.log(11 % 7);
+
+console.log(10% 3);
+
+const first = "Mobarok";
+const secend = "Tobarok";
+console.log(first + secend);
+
+// Warning 
+const sugar = 20 ;
+const daddy = "20" ;
+console.log(sugar + daddy);
+
+const price1 = 35;
+const inboxEAsenPrice = price1 + 10;
+const newPrice = price1 + 50 ;
+const discountedPrice = newPrice - 10 ;
+console.log(discountedPrice)
+
+// Practice 
+const income = 100;
+const expense = 40;
+const savings = income - expense ;
+console.log(savings);
+ 
+const pancil = 10 ;
+const  usePencil = 2 ;
+const  use = pancil / usePencil ;
+console.log(use); 
+
+const money = 60 ;
+const costPerTime = 15 ;
+const useMoney = money / costPerTime ;
+console.log(useMoney);
+const remainder = money % costPerTime ;
+console.log(remainder);
+
+const hello = "Hello" ;
+const world = "World" ;
+const helloWorld = hello + world ;
+console.log(helloWorld);
+
+const rice = 153 ;
+const dal = 261 ;
+const riceDal = rice + dal ;
+console.log(riceDal);
+
+const taka = 500 ;
+const perTimeCost = 75 ;
+const costTotal = taka / perTimeCost ;
+console.log(costTotal);
+const remainderTaka = taka % perTimeCost ;
+console.log(remainderTaka);
+
+const number8 = 8 ;
+const number3 = 3 ;
+const numberDivide = number8 / number3 ;
+console.log(numberDivide);
+const numberRemainder = number8 % number3 ;
+console.log(numberRemainder);
+
+const number50 = 50 ;
+const number9 = 9 ;
+const divideNumber = number50 % number9 ;
+console.log(divideNumber);
+
+const bangla = "Bangla" ;
+const desh = "desh" ;
+const bangladesh = bangla + desh ;
+console.log(bangladesh);
+
+const everydayEatRice = 4 ;
+const givenRice = 480 ;
+const month = 30 ;
+const riceEnd = givenRice / everydayEatRice ;
+console.log(riceEnd);
+const eatRiceMonth = everydayEatRice * month ;
+console.log(eatRiceMonth);
+const finishRice = givenRice / eatRiceMonth ;
+console.log(finishRice);
