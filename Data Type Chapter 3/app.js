b1d00ed09955a43b122ea6c5f85fa21465ff9638
Bayezid Hossain <bayezidhossain086@gmail.com>
2025-05-16 18:39:32 +0600

    Data Type Chapter 3

---
+++ Data Type Chapter 3/app.js
@@ -0,0 +1,194 @@
+// typeof
+const passed = false;
+console.log(typeof passed);
+
+const country = "Bangladesh";
+console.log(typeof country);
+
+const priceEgg = 100 ;
+console.log(typeof priceEgg);
+
+// Practice
+const price = 555 ;
+console.log(typeof price);
+
+const learning = true ;
+console.log(learning);
+console.log (typeof learning);
+
+const megaCity = "Dhaka";
+console.log(typeof megaCity);
+
+const temperature = 102.5 ;
+console.log(typeof temperature);
+
+const isHungry = true ;
+console.log(typeof isHungry);
+
+const isRich = true ;
+console.log(typeof isRich);
+
+// let
+
+let pricerice = 200;
+pricerice = 300;
+console.log(pricerice);
+
+let balance = 500;
+balance = 400;
+console.log(balance);
+
+// Practice
+
+let age = 17 ;
+age = 18 ;
+console.log(age);
+
+let pocketMoney = 500 ;
+pocketMoney = 150 ;
+console.log(pocketMoney);
+
+let studyTime = 4 ;
+studyTime = 1 ;
+console.log(studyTime);
+
+let friendsCount =  3 ;
+friendsCount = 1 ;
+console.log(friendsCount);
+
+let phonePrice = 25000 ;
+phonePrice = 12000 ;
+console.log(phonePrice);
+
+let chairCount = 40 ;
+chairCount = 32  ;
+console.log(chairCount);
+
+let playTime = 3 ;
+playTime =  2 ;
+console.log(playTime);
+
+// Comment
+
+// score IELTS
+let score = 7.5 ;
+
+// border radius
+let radius = 5 ;
+
+/* My favourite dress */ 
+let dress = 4 ;
+
+// I got a gift 
+let giftMoney = 100 ;
+
+/* Bangla 
+English 
+Math
+JavaScript */
+let timeStudy = 4; 
+
+/* 1000
+   Twenty taka per thousand
+ */
+let sendMoney = 1000 ;
+
+// Price 450 , Delebary charge 50  
+let pay =- 500 ;
+
+// JavaScript Keyword
+// Practice
+
+let favouriteFood = "camelMeat" ;
+
+let city ="Chittagonng";
+console.log(city);
+let City = "Dhaka" ;
+console.log(City);
+
+let userName = "Johan" ;
+console.log(userName);
+let username = "Jovan" ;
+console.log(username);
+
+let myHomeAddress = "Lakshmipur" ;
+// I will not write variables with (sper) name.
+let  superover = 100 ;
+// I will not write variables with (class) name.
+let classlevel = 11 ;
+
+// Variable names cannot start with a number.
+let nd2Position = "Mridul" ;
+
+// Parse-এর ভিতরে নানরুটি (NaN)
+
+ const fatherAge = parseInt ("50") ;
+ const sonAge = parseInt ("20");
+ const totalAge = fatherAge + sonAge ;
+ console.log(  totalAge);
+
+const father = parseInt ('x20')
+console.log(father);
+
+const ag = parseInt ('ajhiojohdfoijf');
+const weight = parseInt ('twenty');
+console.log(ag , weight);
+
+const ae = parseInt ('50f')
+const weigh = parseInt ('9twenty');
+const address = parseInt ('tom60'); 
+console.log(ae , weigh , address);
+
+const sugar = parseInt ('1.22222');
+console.log(sugar);
+
+const sugar1 = parseFloat ('1.222222')
+console.log(sugar1);
+
+const first = 12.13568 ;
+const second = 31.22564 ;
+const total = first + second ;
+console.log(total.toFixed(2));
+
+const first1 = 12.13568 ;
+const second1 = 31.22564 ;
+const total1 = first1 + second1 ;
+const totalNumber = parseFloat(total.toFixed(2));
+console.log(totalNumber);
+
+
+// practice
+
+
+const math =  '20';
+const mathNumber  = parseInt(20);
+const mathtotal = mathNumber + 10 ;
+console.log(mathtotal);
+
+const number = 3.14159 ;
+const numberInteger = parseInt('3.14159');
+console.log(numberInteger);
+
+const boy = "premikBoy";
+const boynumber = parseInt ('premikBoy');
+console.log(boynumber);
+
+const number3 = 3.456 ;
+const number4 = 2.789 ;
+const numbertotal = number3 + number4 ;
+const numberto = parseFloat (numbertotal.toFixed(2));
+console.log(numberto);
+
+const string = "56.78";
+const string1 = "12.34";
+const numberString = parseFloat(string);
+const numberString1 = parseFloat(string1);
+console.log(numberString + numberString1);
+
+const num = 10.5678 ;
+const num1 = 5.4321 ;
+const numtotal = num + num1 ;
+console.log(numtotal.toFixed(1));
+
+const numb = 43.45689754 ;
+console.log(numb.toFixed(3));
