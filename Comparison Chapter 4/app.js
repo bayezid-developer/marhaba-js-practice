5e65d1d9ef2fcf4d77abbb3f744aae63019f76e4
Bayezid Hossain <bayezidhossain086@gmail.com>
2025-07-02 10:07:33 -0700

    Comparson Chapter 4

---
+++ Comparison Chapter 4/app.js
@@ -0,0 +1,126 @@
+// পাশের বাসার আন্টির তুলনা 
+console.log(5>10);
+console.log(5<10);
+console.log( 5 == 10);
+console.log(10 == 10);
+console.log(10 >= 10);
+console.log(15 >= 10);
+console.log(5<= 10);
+console.log(10<= 10);
+console.log(7 != 10);
+// Practice
+const sandwichBayezid = 7 ;
+const sandwichMehil = 5 ;
+console.log(sandwichBayezid > sandwichMehil);
+
+const myclass = 45 ;
+const nextclass = 50 ;
+console.log(myclass < nextclass) ;
+
+const myNumber = 75 ;
+const friendNumber = 75 ;
+console.log(myNumber == friendNumber);
+
+const mytarget = 100 ;
+const filuptarget = 95 ;
+console.log(mytarget >= filuptarget);
+
+console.log( 10  != 7 );
+
+console.log( 20 >= 15);
+
+const myfriendResult = 97 ;
+const myResult = 45 ;
+console.log( myfriendResult < myResult);
+
+// ভেরিয়েবলের তুলনা কখনো ভুলো না 
+
+const peyaraPrice = 40 ;
+const applePrice = 250 ;
+console.log(peyaraPrice > applePrice);
+console.log(peyaraPrice < applePrice);
+
+//  স্ট্রিং বুলিযানের তুলনা 
+
+const myName = 'Rahim' ;
+const friendName = 'Karim' ;
+console.log(myName == friendName);
+
+const myPromise = true ;
+const friendPromise = false ;
+console.log(myPromise == friendPromise);
+
+const didStudy = true ;
+console.log(didStudy != false);
+
+// Practice
+const mySmallBrotherAge = 10 ;
+const myAge = 15 ;
+console.log(mySmallBrotherAge <= myAge);
+
+const passMark = 33 ;
+const myMark = 45 ;
+console.log(myMark > passMark );
+
+const jamburPrice = 67 ;
+const codbalPrice = 80 ;
+console.log(jamburPrice < codbalPrice);
+
+const myFriendRead = 10 ;
+const myRead = 10 ;
+console.log(myFriendRead == myRead);
+
+const mybiscut = 15 ;
+const myFriendBuscut = 20;
+console.log(mybiscut < myFriendBuscut);
+
+const reyalMadrid = 2 ;
+const borsa = 3 ;
+console.log(reyalMadrid < borsa);
+
+const myNmae = "Rahim";
+const myfriendName = "karim";
+console.log(myNmae == myfriendName);
+
+const myStudy = true ;
+const friendStudy = false ;
+console.log(myStudy == friendStudy);
+
+// ডাবল না কি ট্রিপল === (এডভান্সড তুলনা)
+
+console.log(10 == "10");
+console.log(10 === "10");
+console.log(120 === "tomoto");
+
+console.log(10 != 10 );
+console.log(10 != "10");
+console.log(10 !== "10")
+
+// Practice
+
+const mytaka = 50  ;
+const myFrindTaka = "50" ;
+console.log(mytaka == myFrindTaka );
+console.log( mytaka === myFrindTaka );
+
+console.log("JavaScript" == 'JavaScript');
+console.log("JavaScript" === 'JavaScript');
+
+console.log(25 == "25");
+console.log(25 === "25");   // Value and Data type এক নয় । 
+
+console.log("apple" == "apple");
+console.log("apple" === "apple");
+
+console.log("test" == "TEST");
+console.log("test" === "TEST");
+
+ const variableX = 15 ;
+ const variableY = 20 ;
+ console.log(variableX != variableY);
+ console.log(variableX !== variableY);
+
+ const variableA = "hello";
+ const variableB = "Hello";
+console.log(variableA == variableB);
+console.log(variableA === variableB);
